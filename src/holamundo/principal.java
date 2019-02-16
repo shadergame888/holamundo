dd6cf0ad8855de5915f09f83e7e1cf5fae8c9031
shadergame888 <shadergame888@gmail.com>
2019-02-15 19:06:05 -0500

    Agrego otra linea

--- src/holamundo/principal.java
+++ src/holamundo/principal.java
@@ -5,6 +5,8 @@ public class principal {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hola mundo java");
+		
+		System.out.println("Agrego otra linea");
 	}
 
 }
